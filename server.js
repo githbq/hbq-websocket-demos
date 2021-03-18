b1e2a9d47c4fae3511f2be2c96227daa2f294bb5
hubiqing <hubiqing@jd.com>
2021-03-18 10:12:27 +0800

    ignore

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({
+    port: 8080,
+    path: "/server"
+});
+
+wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(message) {
+        console.log(message)
+        if (message === "kill") {
+            // ws.close(1008, "You violated the server policy.");
+        } else {
+            ws.send("pong");
+        }
+    });
+});
